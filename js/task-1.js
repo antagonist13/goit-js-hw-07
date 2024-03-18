663ea97a9bd7b700e22eb3e039065ec0dec0076c
antagonist13 <1703shakayaroslaw@gmail.com>
2024-03-18 20:14:11 +0200

    task 1

--- js/task-1.js
+++ js/task-1.js
@@ -1,34 +1,53 @@
-// Перед звільненням розробник зламав вихідний код управління акаунтами користувачів нашого сервісу доставки їжі. Виконай рефакторинг методів об'єкта customer, розставивши відсутні this під час звернення до властивостей об'єкта.
 
-// Використай цей стартовий код і виконай рефакторинг. Після оголошення об'єкта ми додали виклики методів. У консоль будуть виведені результати їх роботи. Будь ласка, нічого там не змінюй.
+const categories = [...document.querySelectorAll('#categories .item')]
+let categoriesQuantity = 0
+for (let i of categories) {
+  categoriesQuantity += 1
+}
+console.log('Number of categories: ' + categoriesQuantity);
 
-const customer = {
-  username: "Mango",
-  balance: 24000,
-  discount: 0.1,
-  orders: ["Burger", "Pizza", "Salad"],
-  // Change code below this line
-  getBalance() {
-    return this.balance;
-  },
-  getDiscount() {
-    return this.discount;
-  },
-  setDiscount(value) {
-    this.discount = value;
-  },
-  getOrders() {
-    return this.orders;
-  },
-  addOrder(cost, order) {
-    this.balance -= cost - cost * this.discount;
-    this.orders.push(order);
-  },
-  // Change code above this line
-};
+const firstCategory = categories[0]
+console.log('Category: ' + firstCategory.firstElementChild.textContent);
 
-customer.setDiscount(0.15);
-console.log(customer.getDiscount()); // 0.15
-customer.addOrder(5000, "Steak");
-console.log(customer.getBalance()); // 19750
-console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
+const firstCategoryElements = firstCategory.lastElementChild.children
+let firstCategoryElementsQuantity = 0
+let firstCategoryElementsCalculation = [...firstCategoryElements].forEach( (el) => firstCategoryElementsQuantity += 1)
+
+console.log(`Elements: ${firstCategoryElementsQuantity}`);
+
+const secondCategory = categories[1]
+console.log('Category: ' + secondCategory.firstElementChild.textContent);
+
+const secondCategoryElements = secondCategory.lastElementChild.children
+let secondCategoryElementsQuantity = 0
+let secondCategoryElementsCalculation = [...secondCategoryElements].forEach((el) => secondCategoryElementsQuantity += 1)
+
+console.log(`Elements: ${secondCategoryElementsQuantity}`);
+
+const thirdCategory = categories[2]
+console.log('Category: ' + thirdCategory.firstElementChild.textContent);
+
+const thirdCategoryElements = thirdCategory.lastElementChild.children
+let thirdCategoryElementsQuantity = 0
+let thirdCategoryElementsCalculation = [...thirdCategoryElements].forEach((el) => thirdCategoryElementsQuantity += 1)
+
+console.log(`Elements: ${thirdCategoryElementsQuantity}`);
+
+
+
+
+
+
+
+
+
+
+
+
+// Number of categories: 3
+// Category: Animals
+// Elements: 4
+// Category: Products
+// Elements: 3
+// Category: Technologies 
+// Elements: 5
