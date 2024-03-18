ada7b9eb206e52c56f517b47e03d029ce0669c47
antagonist13 <1703shakayaroslaw@gmail.com>
2024-03-18 20:50:22 +0200

    task 3 cor

--- js/task-3.js
+++ js/task-3.js
@@ -2,6 +2,6 @@ const textInput = document.querySelector("#name-input");
 const textOutput = document.querySelector("#name-output");
 
 textInput.addEventListener("input", (event) => {
-    if (event.currentTarget.value === "" || event.currentTarget.value === " ") return
+    if (event.currentTarget.value === "" || event.currentTarget.value.trim().length === 0 )return
   textOutput.textContent = event.currentTarget.value.trim()
 });
