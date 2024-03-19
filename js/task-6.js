9e7d5f905de665f69dc04bfc3a3890376acef77a
antagonist13 <1703shakayaroslaw@gmail.com>
2024-03-19 21:09:11 +0200

    task 6 add

--- js/task-6.js
+++ js/task-6.js
@@ -4,17 +4,18 @@ const createBtn = document.querySelector('button[data-create]')
 const destroyBtn = document.querySelector('button[data-destroy]')
 const boxes = document.querySelector("#boxes")
 
-let quantity = 0
-
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
 
-function newBoxCreating (event) {
-   boxes.insertAdjacentHTML("beforeend", '<div class = "box"></div>')
+function newBoxCreating(event) {
+    let step = 0
+    for (let i = 0; i < Number(number.value); i++) {
+        step += 10
+        boxes.insertAdjacentHTML("beforeend", '<div class = "box"></div>')
+    }
 }
-
 createBtn.addEventListener("click", newBoxCreating)
 
