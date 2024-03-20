bfc1a3d130b1ade2d05609fbd474b0ae5232a635
antagonist13 <1703shakayaroslaw@gmail.com>
2024-03-20 15:03:32 +0200

    fix 6

--- js/task-6.js
+++ js/task-6.js
@@ -4,18 +4,26 @@ const createBtn = document.querySelector('button[data-create]')
 const destroyBtn = document.querySelector('button[data-destroy]')
 const boxes = document.querySelector("#boxes")
 
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
-}
+
 
 function newBoxCreating(event) {
-    let step = 0
+    let previousSize = 10
+    if (number.value > 100 || number.value < 1) {
+        return
+    }
     for (let i = 0; i < Number(number.value); i++) {
-        step += 10
-        boxes.insertAdjacentHTML("beforeend", '<div class = "box"></div>')
+        previousSize += 10
+        function getRandomHexColor() {
+            return `#${Math.floor(Math.random() * 16777215)
+            .toString(16)
+            .padStart(6, 0)}`;
+        }
+        const color = getRandomHexColor()
+        boxes.insertAdjacentHTML("beforeend", "<div class='box' style='width: " + (previousSize + 10) + "px; height: " + (previousSize + 10) + "px; background-color:" + color + ";' ></div>")
     }
 }
+function destroyBox(event) {
+    boxes.innerHTML = ''
+}
 createBtn.addEventListener("click", newBoxCreating)
-
+destroyBtn.addEventListener("click", destroyBox)
