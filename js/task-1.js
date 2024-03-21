c4433b1b301d8496acf8689a76602a2b4f5830e5
antagonist13 <1703shakayaroslaw@gmail.com>
2024-03-21 12:29:20 +0200

    task 2 refactoring

--- js/task-1.js
+++ js/task-1.js
@@ -1,10 +1,13 @@
+const categories = document.querySelectorAll(".item")
+console.log(`Number of categories: ${categories.length}`);
+console.log(`Category: ${categories[0].firstElementChild.textContent}`);
+console.log(`Elements: ${categories[0].lastElementChild.children.length}`);
 
+console.log(`Category: ${categories[1].firstElementChild.textContent}`);
+console.log(`Elements: ${categories[1].lastElementChild.children.length}`);
 
-
-
-
-
-
+console.log(`Category: ${categories[2].firstElementChild.textContent}`);
+console.log(`Elements: ${categories[2].lastElementChild.children.length}`);
 
 // Number of categories: 3
 // Category: Animals
@@ -18,7 +21,7 @@ const headings = document.querySelectorAll("h2")
 headings.forEach((el) => el.classList.add("header"))
 
 let allLi = []
-const li = (allLi.push([...firstCategoryElements],[...secondCategoryElements], [...thirdCategoryElements]) )
+const li = (allLi.push([...categories[0].lastElementChild.children],[...categories[1].lastElementChild.children], [...categories[2].lastElementChild.children]) )
 
 for (const li of allLi) {
   li.forEach((el) => el.classList.add("text"))
