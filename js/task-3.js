2693761920ad276420f5666dd6250f961c4eb029
antagonist13 <1703shakayaroslaw@gmail.com>
2024-03-21 12:33:02 +0200

    task 3 correction

--- js/task-3.js
+++ js/task-3.js
@@ -2,10 +2,14 @@ const textInput = document.querySelector("#name-input");
 const textOutput = document.querySelector("#name-output");
 
 textInput.addEventListener("input", (event) => {
-    if (event.currentTarget.value === "" || event.currentTarget.value.trim().length === 0 )return
-  textOutput.textContent = event.currentTarget.value.trim()
+    const trimmedValue = event.currentTarget.value.trim();
+    textOutput.textContent = (trimmedValue === "") ? "Anonymous" : trimmedValue;
 });
 
 const mainText = document.querySelector("h1")
 mainText.classList.add("header")
-textInput.classList.add("window","text")
+textInput.classList.add("window", "text")
+
+
+
+
